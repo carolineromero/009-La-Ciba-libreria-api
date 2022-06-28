9dd04441c93c2d6b89f873de8a802eb407d32e0a
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-06-28 09:21:50 +0200

    carpets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import navBar from './components/header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola 
-        </a>
-      </header>
-    </div>
+    <header/>
   );
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function navBar () {
+    return (
+      <header>
+        <div className="Navbar">
+          <img src= "" className="Library-Logo" alt="biblioteca-logo" />
+          <a className="Home" href="" target= "_blank"  rel="noopener noreferrer">Home</a>
+          <a className="Create" href="" target="_blank" rel="noopener noreferrer">Create</a>
+          <a className="Registration" href="" target="_blank" rel="noopener noreferrer">Registration</a>
+       </div>
+      </header>
+    );
+  }
+
+export default navBar
