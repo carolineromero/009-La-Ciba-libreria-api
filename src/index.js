67df894fcaf9481e32877a68ac7fe93ab779c485
caroolromero <carolf5laci@gmail.com>
2022-06-28 13:35:37 +0200

    cambios HOME

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import  { createRoot } from 'react-dom/client';
 import Router from './aplication/Router';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import '../src/styles/styleHome.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
