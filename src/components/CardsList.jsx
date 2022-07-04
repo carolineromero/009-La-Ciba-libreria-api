16a1584022e2f97165f0fd14d3688af88e23bae7
caroolromero <carolf5laci@gmail.com>
2022-07-04 11:28:27 +0200

    cambio 2 returns

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -3,14 +3,14 @@ import bookfile from "../assets/data/books.json";
 import '../styles/styleItemBook.css'
 
 export default function ListCards() {
-    const bookList = bookfile.books.map (item => { 
-        return (
-            <CardBook bookName={item.bookName} author={item.author} genre={item.genre} year={item.year} editorial={item.editorial} image={item.image}/>
-        )
-    })
+  
     return (
         <main className="list-books">
-            {bookList}
+           {
+            bookfile.books.map (item =>(
+                <CardBook bookName={item.bookName} author={item.author} genre={item.genre} year={item.year} editorial={item.editorial} image={item.image}/>
+            ))
+           }
         </main>
     );
 }
