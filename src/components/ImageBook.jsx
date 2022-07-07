24d25bd34eeb1d39826098e570fce1fcbe734161
caroolromero <carolf5laci@gmail.com>
2022-07-07 12:29:36 +0200

    covers ok

---
+++ src/components/ImageBook.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+function ImageBook ({isbn}){
+    
+    const [imgBook, setImgBook] = useState([]);
+    useEffect (()=>{
+        fetch(`https://openlibrary.org/isbn/${isbn}.json`)
+        .then (response=>response.json())
+        .then (data=>setImgBook(data))
+    }, [])
+    if (typeof imgBook === 'object'){
+        if (imgBook.covers === undefined || imgBook.covers === null){
+            return(
+                <img className="bookCover" src={"https://as2.ftcdn.net/v2/jpg/01/98/86/63/1000_F_198866399_vQambmqMEK9975X1Yg7686t4nFpSaubL.jpg"} alt ="imagen de libro"/>
+            )
+        } else {
+            return(
+                <img className="bookCover" src={`https://covers.openlibrary.org/b/id/${imgBook.covers}.jpg`} alt ="imagen de libro"/>
+            )
+        }
+    } else {
+        return(
+            <img className="bookCover" src={"https://as2.ftcdn.net/v2/jpg/01/98/86/63/1000_F_198866399_vQambmqMEK9975X1Yg7686t4nFpSaubL.jpg"} alt ="imagen de libro"/>
+        )
+    }
+}
+export default ImageBook;
